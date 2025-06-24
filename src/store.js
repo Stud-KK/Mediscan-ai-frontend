f5ce738057c2353c0288b54715334468c20770bc
Komal Kalshetti <komalkalshetti989@gmail.com>
2025-06-25 00:45:20 +0530

    add files

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { thunk } from 'redux-thunk'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';  // redux-persist for storing data in state
